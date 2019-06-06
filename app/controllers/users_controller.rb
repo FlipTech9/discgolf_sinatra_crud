143fbd87171ae21d33908f228c231ce1dfc3fb7f
FlipTech9 <38243324+FlipTech9@users.noreply.github.com>
2019-06-06 05:05:59 -0400

    completed User model, view, and controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,44 @@
+class UsersController < ApplicationController
+
+    get '/signup' do 
+        if logged_in?
+            redirect '/discs'
+        else 
+            erb :'/users/create_user'
+        end 
+    end 
+
+    post '/signup' do 
+        if params["username"] != "" && params["password"] != ""
+            @user.User.create(params)
+            @user.save 
+            session[:user_id] = @user.id 
+            redirect '/discs'
+        end 
+        "Please fill required fields"
+        redirect '/signup'
+        
+    end 
+
+    get '/login' do 
+        if logged_in?
+            redirect '/discs'
+        else 
+            erb :'users/login'
+        end 
+
+    end 
+
+    post '/login' do 
+        @user = User.find_by(username: params[:username])
+        session[:user_id] = @user.id 
+        redirect '/discs'
+    end 
+
+    get '/logout' do 
+        session.clear 
+        redirect '/login'
+    end 
+    
+
+end 
